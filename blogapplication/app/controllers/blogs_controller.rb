190ed428206914c0e2306b3301f7b5cdb8da84b7
Dipayan Dutta <inbox.dipayan@gmail.com>
2020-04-03 17:59:09 +0530

    How to add bootstrap in Rails Application

---
+++ blogapplication/app/controllers/blogs_controller.rb
@@ -0,0 +1,19 @@
+class BlogsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def delete
+  end 
+
+  def delete
+  end 
+
+  def new 
+  end
+
+  def edit
+  end 
+end
---
+++ blogapplication/config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  #get 'blogs/index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'blogs#index'
+  #get 'blogs/new'
+  get 'new'=>'blogs#new'
+end
